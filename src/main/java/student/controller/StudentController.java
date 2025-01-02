6dfd4ee202ccbe614066b95064c62d58d4e437fd
madsensss <madsenss@naver.com>
2025-01-02 14:22:08 +0900

    Update StudentController.java

--- src/main/java/student/controller/StudentController.java
+++ src/main/java/student/controller/StudentController.java
@@ -26,7 +26,7 @@ public class StudentController {
     
     @GetMapping("/hello")
     public String sayHello() {
-        return "hello ecs";
+        return "hello ecs CICD";
     }
     
     @GetMapping
